4fb48c87141c25a4cd4c1ca156e40f240eecdd9a
mantissss <sebesteanattilo@yahoo.com>
2022-01-15 14:57:46 +0200

    Lab9

---
+++ Models/BookData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sebestean_Balazs_Attila_Lab8.Models
+{
+    public class BookData
+    {
+        public IEnumerable<Book> Books { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<BookCategory> BookCategories { get; set; }
+    }
+}
